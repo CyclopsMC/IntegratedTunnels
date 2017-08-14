a8403f8ea0225112456af241c1794a13a01ff934
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-08-14 07:25:48 -0400

    Restore the null check but with a return value of 0

--- src/main/java/org/cyclops/integratedtunnels/part/PartStateItem.java
+++ src/main/java/org/cyclops/integratedtunnels/part/PartStateItem.java
@@ -39,6 +39,6 @@ public class PartStateItem<P extends IPartTypeWriter> extends PartStatePositione
 
     @Override
     public int getSlotLimit(int slot) {
-        return getPositionedAddonsNetwork().getSlotLimit(slot);
+        return getPositionedAddonsNetwork() != null ? getPositionedAddonsNetwork().getSlotLimit(slot) : 0;
     }
 }
