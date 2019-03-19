053dfc45563d3f633f1d12a87fc2dd67d0eff0fe
Ruben Taelman <rubensworks@gmail.com>
2019-03-19 17:09:42 +0100

    Bump mod version

--- src/main/java/org/cyclops/integratedtunnels/Reference.java
+++ src/main/java/org/cyclops/integratedtunnels/Reference.java
@@ -36,9 +36,9 @@ public class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "14.23.5.2768";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "1.0.6";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "1.1.0";
     public static final String MOD_INTEGRATEDDYNAMICS = "integrateddynamics";
-    public static final String MOD_INTEGRATEDDYNAMICS_VERSION_MIN = "1.0.4";
+    public static final String MOD_INTEGRATEDDYNAMICS_VERSION_MIN = "1.0.9";
     public static final String MOD_TESLA = "tesla";
     
     // Dependencies
