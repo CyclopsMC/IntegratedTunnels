CyclopsMC/IntegratedTunnels

commit 8f659ea686fed797b195c50ddb81708af7fc8623

Fix predicate-based transfer not considering slots
Predicates were being applied to the network's index instead
of separate ingredient slots, which would cause ingredients to
be stacked, and predicates not always receiving the expected inputs.

Closes #271

src/main/java/org/cyclops/integratedtunnels/part/aspect/FluidTargetCapabilityProvider.java
@@ -48,7 +48,12 @@ public class FluidTargetCapabilityProvider extends ChanneledTargetCapabilityProv
 
     @Override
     public IIngredientComponentStorage<FluidStack, Integer> getFluidChannel() {
-        return getChanneledNetwork().getChannel(getChannel());
+        if (getFluidStackMatcher().hasMatchFlags()) {
+            return getChanneledNetwork().getChannel(getChannel());
+        }
+        // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
+        // as the predicate must apply to each slotted ingredient.
+        return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 
     @Override

src/main/java/org/cyclops/integratedtunnels/part/aspect/ItemTargetCapabilityProvider.java
@@ -1,7 +1,7 @@
 package org.cyclops.integratedtunnels.part.aspect;
 
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
@@ -45,7 +45,12 @@ public class ItemTargetCapabilityProvider extends ChanneledTargetCapabilityProvi
 
     @Override
     public IIngredientComponentStorage<ItemStack, Integer> getItemChannel() {
-        return getChanneledNetwork().getChannel(getChannel());
+        if (getItemStackMatcher().hasMatchFlags()) {
+            return getChanneledNetwork().getChannel(getChannel());
+        }
+        // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
+        // as the predicate must apply to each slotted ingredient.
+        return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 
     @Override

gradle.properties
@@ -1,13 +1,13 @@
 mod_version=1.8.18
 minecraft_version=1.18.2
 forge_version=40.0.54
-cyclopscore_version=1.13.4-158
-integrateddynamics_version=1.18.2-1.10.13-249
+cyclopscore_version=1.17.0-292
+integrateddynamics_version=1.18.2-1.14.0-626
 integratedtunnelscompat_version=1.18.2-1.0.0-16
 release_type=release
 fingerprint=bd0353b3e8a2810d60dd584e256e364bc3bedd44
 
-commoncapabilities_version=1.18.2-2.8.2-54
+commoncapabilities_version=1.18.2-2.9.0-86
 
 # Workaround for Spotless bug
 # https://github.com/diffplug/spotless/issues/834

commit fe31ada0897e1907342c14f684059b4592cde218

Fix predicate-based importing ignoring round-robin
This bug was introduced in 8f659ea686fed797b195c50ddb81708af7fc8623
as a fix for #271. That commit enforced slot-based iteration over
the network, which ignores round-robin-based iterators. That fix
actually only needed to be applied to exporting, which is what this
commit does now.

Closes #288

src/main/java/org/cyclops/integratedtunnels/part/aspect/FluidTargetCapabilityProvider.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.FluidStack;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.api.part.aspect.property.IAspectProperties;
@@ -48,11 +49,11 @@ public class FluidTargetCapabilityProvider extends ChanneledTargetCapabilityProv
 
     @Override
     public IIngredientComponentStorage<FluidStack, Integer> getFluidChannel() {
-        if (getFluidStackMatcher().hasMatchFlags()) {
-            return getChanneledNetwork().getChannel(getChannel());
-        }
-        // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
-        // as the predicate must apply to each slotted ingredient.
+        return getChanneledNetwork().getChannel(getChannel());
+    }
+
+    @Override
+    public IIngredientComponentStorageSlotted<FluidStack, Integer> getFluidChannelSlotted() {
         return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 

src/main/java/org/cyclops/integratedtunnels/part/aspect/FluidTargetStorage.java
@@ -2,6 +2,7 @@ package org.cyclops.integratedtunnels.part.aspect;
 
 import net.minecraftforge.fluids.FluidStack;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.api.part.aspect.property.IAspectProperties;
@@ -42,6 +43,11 @@ public class FluidTargetStorage extends ChanneledTarget<IFluidNetwork, FluidStac
     @Override
     public IIngredientComponentStorage<FluidStack, Integer> getFluidChannel() {
         return getChanneledNetwork().getChannel(getChannel());
+    }
+
+    @Override
+    public IIngredientComponentStorageSlotted<FluidStack, Integer> getFluidChannelSlotted() {
+        return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 
     @Override

src/main/java/org/cyclops/integratedtunnels/part/aspect/IFluidTarget.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.fluids.FluidStack;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
@@ -20,6 +21,8 @@ import javax.annotation.Nullable;
 public interface IFluidTarget extends IChanneledTarget<IFluidNetwork, FluidStack> {
 
     public IIngredientComponentStorage<FluidStack, Integer> getFluidChannel();
+
+    public IIngredientComponentStorageSlotted<FluidStack, Integer> getFluidChannelSlotted();
 
     public IIngredientComponentStorage<FluidStack, Integer> getStorage();
 

src/main/java/org/cyclops/integratedtunnels/part/aspect/IItemTarget.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
@@ -20,6 +21,8 @@ import javax.annotation.Nullable;
 public interface IItemTarget extends IChanneledTarget<IItemNetwork, ItemStack> {
 
     public IIngredientComponentStorage<ItemStack, Integer> getItemChannel();
+
+    public IIngredientComponentStorageSlotted<ItemStack, Integer> getItemChannelSlotted();
 
     public IIngredientComponentStorage<ItemStack, Integer> getStorage();
 

src/main/java/org/cyclops/integratedtunnels/part/aspect/ItemTargetCapabilityProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.api.part.aspect.property.IAspectProperties;
@@ -45,11 +46,11 @@ public class ItemTargetCapabilityProvider extends ChanneledTargetCapabilityProvi
 
     @Override
     public IIngredientComponentStorage<ItemStack, Integer> getItemChannel() {
-        if (getItemStackMatcher().hasMatchFlags()) {
-            return getChanneledNetwork().getChannel(getChannel());
-        }
-        // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
-        // as the predicate must apply to each slotted ingredient.
+        return getChanneledNetwork().getChannel(getChannel());
+    }
+
+    @Override
+    public IIngredientComponentStorageSlotted<ItemStack, Integer> getItemChannelSlotted() {
         return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 

src/main/java/org/cyclops/integratedtunnels/part/aspect/ItemTargetStorage.java
@@ -2,6 +2,7 @@ package org.cyclops.integratedtunnels.part.aspect;
 
 import net.minecraft.world.item.ItemStack;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.api.part.aspect.property.IAspectProperties;
@@ -44,6 +45,11 @@ public class ItemTargetStorage extends ChanneledTarget<IItemNetwork, ItemStack> 
     @Override
     public IIngredientComponentStorage<ItemStack, Integer> getItemChannel() {
         return getChanneledNetwork().getChannel(getChannel());
+    }
+
+    @Override
+    public IIngredientComponentStorageSlotted<ItemStack, Integer> getItemChannelSlotted() {
+        return getChanneledNetwork().getChannelSlotted(getChannel());
     }
 
     @Override

src/main/java/org/cyclops/integratedtunnels/part/aspect/TunnelAspectWriteBuilders.java
@@ -1,16 +1,16 @@
 package org.cyclops.integratedtunnels.part.aspect;
 
 import com.google.common.collect.ImmutableList;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.nbt.Tag;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.phys.AABB;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
@@ -786,12 +786,16 @@ public class TunnelAspectWriteBuilders {
 
             if (input.hasValidTarget()) {
                 input.preTransfer();
+                // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
+                // as the predicate must apply to each slotted ingredient.
+                // Only do this for exporting, not for importing, as this would otherwise break round-robin imports.
+                IIngredientComponentStorage<ItemStack, Integer> source = input.getItemStackMatcher().hasMatchFlags() ? input.getItemChannel() : input.getItemChannelSlotted();
                 TunnelHelpers.moveSingleStateOptimized(
                         input.getNetwork(),
                         input.getChanneledNetwork(),
                         input.getChannel(),
                         input.getConnection(),
-                        input.getItemChannel(), -1,
+                        source, -1,
                         input.getStorage(), input.getSlot(),
                         input.getItemStackMatcher(),
                         input.getPartTarget().getCenter(),
@@ -1152,12 +1156,16 @@ public class TunnelAspectWriteBuilders {
 
             if (input.hasValidTarget()) {
                 input.preTransfer();
+                // For predicate-based matchers, make sure we can iterate over the contents in a slotted manner,
+                // as the predicate must apply to each slotted ingredient.
+                // Only do this for exporting, not for importing, as this would otherwise break round-robin imports.
+                IIngredientComponentStorage<FluidStack, Integer> source = input.getFluidStackMatcher().hasMatchFlags() ? input.getFluidChannel() : input.getFluidChannelSlotted();
                 TunnelHelpers.moveSingleStateOptimized(
                         input.getNetwork(),
                         input.getChanneledNetwork(),
                         input.getChannel(),
                         input.getConnection(),
-                        input.getFluidChannel(),
+                        source,
                         -1,
                         input.getStorage(),
                         -1,