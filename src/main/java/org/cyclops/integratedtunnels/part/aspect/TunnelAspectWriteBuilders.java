202476910ef134c2bfa25b267f166bc5f75a9c3b
Ruben Taelman <rubensworks@gmail.com>
2025-03-29 14:24:46 +0100

    Fix fluid lists not always matching correctly, Closes #333

--- src/main/java/org/cyclops/integratedtunnels/part/aspect/TunnelAspectWriteBuilders.java
+++ src/main/java/org/cyclops/integratedtunnels/part/aspect/TunnelAspectWriteBuilders.java
@@ -1733,7 +1733,7 @@ public class TunnelAspectWriteBuilders {
                 IAspectProperties properties = input.getMiddle();
                 boolean checkNbt = properties.getValue(TunnelAspectWriteBuilders.Fluid.PROP_CHECK_NBT).getRawValue();
                 boolean blacklist = properties.getValue(PROP_BLACKLIST).getRawValue();
-                IngredientPredicate<FluidStack, Integer> fluidStackPredicate = TunnelFluidHelpers.matchFluidStacks(list.getRawValue(), false, false, checkNbt, blacklist, FluidHelpers.BUCKET_VOLUME, true);
+                IngredientPredicate<FluidStack, Integer> fluidStackPredicate = TunnelFluidHelpers.matchFluidStacks(list.getRawValue(), true, false, checkNbt, blacklist, FluidHelpers.BUCKET_VOLUME, true);
                 return IFluidTarget.ofBlock(fluidStackPredicate, input.getLeft(), input.getMiddle(), fluidStackPredicate);
             };
             public static final IAspectValuePropagator<Triple<PartTarget, IAspectProperties, ValueTypeOperator.ValueOperator>, IFluidTarget>
